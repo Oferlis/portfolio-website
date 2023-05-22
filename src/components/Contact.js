2f1f11fa1f1c2a195ec3d29b3fca4d19482a141d
Ofer Lis <oferlis1@gmail.com>
2023-05-22 19:19:10 +0300

    further contact form design

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,24 +8,24 @@ export default function Contact() {
 
     //resource https://www.positronx.io/how-to-build-a-simple-contact-form-in-react-js-app/
 
-    return (<div className="flex h-96 flex-col">
+    return (<div className="flex h-96 flex-col mx-24">
         <h2>
             Contact me
         </h2>
-        <form onSubmit={handleSubmit} className="flex flex-col">
-            <div>
+        <form onSubmit={handleSubmit} className="flex flex-col w-60">
+            <div className="flex flex-col">
                 <label htmlFor="name">Name </label>
                 <input type="text" id="name" required/>
             </div>
-            <div>
+            <div className="flex flex-col">
                 <label htmlFor="email">E-mail </label>
                 <input type="text" id="email" required/>
             </div>
-            <div>
+            <div className="flex flex-col">
                 <label htmlFor="message">Message </label>
-                <input type="text" id="message" required/>                
+                <input className="h-24" type="text" id="message" required/>                
             </div>
-            <button type="submit" className="flex justify-start">
+            <button type="submit" className="flex bg-[#7e8799] rounded-xl justify-center w-16 my-2">
                 {formStatus}
             </button>
         </form>

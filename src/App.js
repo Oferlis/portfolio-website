544362e28050e96a506f1f56fa89209f31c73248
Ofer Lis <oferlis1@gmail.com>
2023-06-05 18:55:07 +0300

    testing new layout, jump to top problems

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Navbar from './components/Navbar';
-import About from './components/About';
-import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import ProjectsPage from './components/ProjectsPage';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-    <main id="home" className="bg-[#EEEFF1] body-font" >
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet"/>
-      <div className= 'flex flex-col'>
-        <Navbar/>
-        <About/>
-        <Skills/>
-        <Projects/>
-        <Contact/>
-        <Footer/>
-      </div>
-    </main>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/projects" element={<ProjectsPage/>}/>
+        </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,26 +10,9 @@ export default function Navbar() {
     }
 
     return (
-    // <header className="flex justify-start place-content-center max-w-5xl font-montserrat mx-36 mt-12 mb-16" width="100%">
-    //     <img src='ofer_pic.png' alt="me" className="" width="100px" height="100px"/>
-    //     <div className="flex flex-col mx-4 my-2">
-    //     <h1 className="py-2 text-3xl text-black">Ofer Lis</h1>
-    //     <div className="flex text-lg">
-    //         <nav className="flex justify-around text-[#5e5e5e]">
-    //             <a className="flex pr-4 my-2" href="/about">About</a>
-    //             <a className="flex px-4 my-2" href="/skills">Skills</a>
-    //             <a className="flex px-4 my-2" href="/project">Projects</a>
-    //             <div className="flex px-4 my-2">
-    //                 <a href="/contact">Say Hi</a>
-    //             </div>
-    //         </nav>
-    //     </div>
-            
-    //     </div>
-    // </header>
         <nav className="border-gray-200 sticky top-0 bg-gray-300" >
             <div id="home" className="max-w-screen-xl flex flex-wrap items-center justify-between md:justify-start mx-auto p-4 font-montserrat ">
-                <HashLink smooth to="/#home">
+                <HashLink smooth to="/">
                 <img src="ofer_pic.png" alt="me" className="h-8 mr-3"/>
                 </HashLink>
                 <button onClick={isShowMenu} type="button" className="inline-flex md:items-center p-2 ml-3 text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
@@ -39,16 +22,10 @@ export default function Navbar() {
                 <div className={ showMenu + " w-full md:block md:w-auto"} id="navbar-default">
                 <ul className="font-medium flex flex-col p-4 md:p-0 md:mx-4 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
                     <li>
-                    <HashLink smooth to="/#home" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 bg-blue-400 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page"> Home </HashLink>
+                    <HashLink smooth to="/" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About</HashLink>
                     </li>
                     <li>
-                    <HashLink smooth to="/#about" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">About</HashLink>
-                    </li>
-                    <li>
-                    <HashLink smooth to="/#skills" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Skills</HashLink>
-                    </li>
-                    <li>
-                    <HashLink smooth to="/#projects" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Projects</HashLink>
+                    <HashLink smooth to="/projects" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">What am I up to?</HashLink>
                     </li>
                     <li>
                     <HashLink smooth to="/#contact" onClick={()=> setShowMenu('hidden')} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Say Hi</HashLink>

251c60b3eb10c1ab2182cd71dbb2d8818b95ce6d
Ofer Lis <oferlis1@gmail.com>
2023-06-29 18:08:25 +0300

    playing with animations

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,19 +7,23 @@ import { portfolioSite } from "../projects-items/portfolioSite"
 
 export default function Projects() {                
     const [displayProject, setDisplayProject] = useState(readingList)
+    let isSelected = false;
+    const handleItemClicked = (item) => {
+        setDisplayProject(item)
+        isSelected = true;
+    }
 
     return (<div id="projects" className="div-ex projects-div">
             <div className="my-auto py-4 w-1/3">
             <h1 className="md:text-4xl text-2xl mb-4 ">My Projects </h1>
             <ul className="">
-                <li className="proj-list-item" onClick={() => setDisplayProject(readingList)}>Reading List</li>
-                <li className="proj-list-item" onClick={() => setDisplayProject(overlay)}>Overlay</li>
-                <li className="proj-list-item" onClick={() => setDisplayProject(sharkio)}>Sharkio</li>
-                <li className="proj-list-item" onClick={() => setDisplayProject(portfolioSite)}>Portfolio site</li>
+                <li className={`proj-list-item ${isSelected ? 'selected' : ''}`} onClick={() => handleItemClicked(readingList)}>Reading List</li>
+                <li className="proj-list-item" onClick={() => handleItemClicked(overlay)}>Overlay</li>
+                <li className="proj-list-item" onClick={() => handleItemClicked(sharkio)}>Sharkio</li>
+                <li className="proj-list-item" onClick={() => handleItemClicked(portfolioSite)}>Portfolio site</li>
 
             </ul>
             </div>
             <ProjectDisplay content={displayProject}/>
-        
     </div>)
 }

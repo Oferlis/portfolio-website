c882ed1a2baddbbd3020dba48eae5de0d940d4ab
Ofer Lis <oferlis1@gmail.com>
2023-07-29 12:27:36 +0300

    added sections seperator, testing new color schemes

--- src/sections/Technologies.js
+++ src/sections/Technologies.js
@@ -13,17 +13,17 @@ export default function Technologies() {
       <div className="logo-div">
         <Logo src={process.env.PUBLIC_URL + "/svgs/django.svg"} id="1" />
         <Logo src={process.env.PUBLIC_URL + "/svgs/react.svg"} id="2" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/ansible.svg"} id="3" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/css.svg"} id="4" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/docker.svg"} id="5" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/git.svg"} id="6" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/github.svg"} id="7" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/html.svg"} id="8" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/nodejs.svg"} id="9" />
-        <Logo src={process.env.PUBLIC_URL + "/svgs/ruby.svg"} id="10" />
         <Logo src={process.env.PUBLIC_URL + "/svgs/typescript.svg"} id="11" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/nodejs.svg"} id="9" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/docker.svg"} id="5" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/html.svg"} id="8" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/css.svg"} id="4" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/ruby.svg"} id="10" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/github.svg"} id="7" />
         <Logo src={process.env.PUBLIC_URL + "/svgs/postgresql.svg"} id="12" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/git.svg"} id="6" />
         <Logo src={process.env.PUBLIC_URL + "/svgs/mongodb.svg"} id="13" />
+        <Logo src={process.env.PUBLIC_URL + "/svgs/ansible.svg"} id="3" />
       </div>
     </div>
   );

674709d996053a54dee23a050c59625a3f307975
Ofer Lis <oferlis1@gmail.com>
2023-08-01 15:46:56 +0300

    form manipulation

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -31,17 +31,9 @@ export default function ContactForm(props) {
           Name
         </label>
         <div className="">
-          <input
-            type="text"
-            name="name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Your name"
-          />
+          <input type="text" name="name" placeholder="Your name" />
         </div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 text-left"
-        >
+        <label htmlFor="email" className="">
           Your Email
         </label>
         <input
@@ -56,12 +48,7 @@ export default function ContactForm(props) {
             {" "}
             Message{" "}
           </label>
-          <input
-            className="h-24 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            type="text"
-            name="message"
-            required
-          />
+          <input type="text" name="message" required />
         </div>
         <div className="">
           <button type="submit">Send</button>

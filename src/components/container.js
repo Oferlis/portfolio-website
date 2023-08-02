7955909da6de69a995585f2c40bde0b2d4be5175
Ofer Lis <oferlis1@gmail.com>
2023-08-02 13:18:37 +0300

    changed colors

--- src/components/container.js
+++ src/components/container.js
@@ -1,7 +1,11 @@
 const Container = (props) => {
   return (
     <div className="box-container">
-      <div className="container">{props.headline}</div>
+      <div className="container">
+        <h2>
+          <span className="highlight">{props.headline}</span>
+        </h2>
+      </div>
       <ul className="container">
         {props.items.map((item) => {
           return <li key={item}>{item}</li>;

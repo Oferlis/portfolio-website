197cbbead9d1e2a05a5bb8f1a006f9be61ada6a0
Ofer Lis <oferlis1@gmail.com>
2023-08-03 16:41:18 +0300

    added pic

--- src/projects-items/overlay.js
+++ src/projects-items/overlay.js
@@ -2,7 +2,7 @@ import genericProject from "./genericProject";
 
 const tech = ["Open-source", "Vue.js", "JS", "Chrome Extension"];
 const body = "Overlay";
-const img = "";
+const img = process.env.PUBLIC_URL + "/images/overlay.jpeg";
 const alt = "overlay";
 const desc = "Helps developers build safer apps by exposing information";
 const link = "https://github.com/os-scar/overlay";
